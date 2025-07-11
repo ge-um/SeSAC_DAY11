6c603785ddc1c70f0ba30c828da4efb2ba4c6f80
금가경 <gageum0@gmail.com>
2025-07-11 16:04:43 +0900

    Initial Commit

---
+++ SeSAC_DAY11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_DAY11
+//
+//  Created by 금가경 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
